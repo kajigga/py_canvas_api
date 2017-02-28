e7af304926e81a13411a01703441b6b360d5d259
K Developer <kajigga@gmail.com>
2017-02-28 09:58:59 -0700

    add description and better README.rst

--- README.rst
+++ README.rst
@@ -1,3 +1,5 @@
-# py_canvas_api
+py_canvas_api
+=============
+
 This is the first round of my attempt at making a simple, easy to understand library for the Canvas API. It is not
 meant for production use in any way at this point.
--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(
     name='py_canvas_api',
     version='0.1',
-    description = 'A very small library for accessing the API for the Canvas LMS.'
+    description = 'A very small library for accessing the API for the Canvas LMS.',
     py_modules=['canvas_api'], #find_packages(),
-    download_url = 'https://github.com/kajigga/py_canvas_api',
+    download_url = 'https://github.com/kajigga/py_canvas_api/tree/0.1',
     keywords = ['canvaslms', 'api'],
     #packages=['icutils'],
     include_package_data=True,
@@ -13,6 +13,5 @@ setup(
 				'requests'
     ],
     test_suite='nose.collector',
-    tests_require=['nose', 'mock'],
+    tests_require=['nose', 'mock']
 )
-
